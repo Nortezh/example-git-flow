b0478e54641602736318a458c31af0840a0eeaf4
Kawakami Toshio <99279119+K-Toshio@users.noreply.github.com>
2024-02-25 10:35:10 +0700

    feat: add more cool rocket (#2)
    * feat: add cool rocket
    
    * feat: add more cool rocket

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(80, () => {
-    console.log('🚀');
+    console.log('🚀🚀🚀');
   });
 }
 bootstrap();
