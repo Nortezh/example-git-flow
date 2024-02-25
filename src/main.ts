54f69db5c3e120b6360c51b09cc9019227c4a9a5
Kawakami Toshio <seminar_lawn.0q@icloud.com>
2024-02-25 09:50:14 +0700

    chore: add server listening console log

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(80);
+  await app.listen(80, () => {
+    console.log('server listen on port 80');
+  });
 }
 bootstrap();
