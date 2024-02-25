ac5183155357262396d30613e9bf3423e7cc66bf
Kawakami Toshio <seminar_lawn.0q@icloud.com>
2024-02-25 09:52:05 +0700

    chore: add cool rocket

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(80, () => {
-    console.log('server listen on port 80');
+    console.log('server listen on port 80 🚀🚀🚀');
   });
 }
 bootstrap();
