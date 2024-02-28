f13cbeec973969b41d5b12a49bb1e3007b3a85ab
Kawakami Toshio <99279119+K-Toshio@users.noreply.github.com>
2024-02-28 10:35:46 +0700

    chore: add more rocket

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(80, () => {
-    console.log('🚀🚀🚀');
+    console.log('🚀🚀🚀🚀');
   });
 }
 bootstrap();
